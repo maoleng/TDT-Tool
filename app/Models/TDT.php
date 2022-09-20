2f251d48485033bed151d5d3ccb800392a270db7
Bùi Hữu Lộc <feature451@gmail.com>
2022-09-20 11:29:06 +0700

    reformat code

--- app/Models/TDT.php
+++ app/Models/TDT.php
@@ -45,17 +45,39 @@ class TDT extends Base
         return $client;
     }
 
-    /**
-     * @throws JsonException
-     */
-    public function getUnitId($value): string
+    public function getUnitId($value)
     {
-        $faculties = json_decode(json_decode('"[{\"MaDonVi\":\"P12\",\"TenDonVi\":\"Ban ký túc xá\",\"SoTin\":1,\"SoTinChuaXem\":1,\"LoaiDonVi\":\"PHONGBAN_TRUNGTAM\"},{\"MaDonVi\":\"5\",\"TenDonVi\":\"Khoa Công nghệ thông tin\",\"SoTin\":90,\"SoTinChuaXem\":58,\"LoaiDonVi\":\"KHOA\"},{\"MaDonVi\":\"B\",\"TenDonVi\":\"Khoa Tài chính ngân hàng\",\"SoTin\":7,\"SoTinChuaXem\":6,\"LoaiDonVi\":\"KHOA\"},{\"MaDonVi\":\"P02\",\"TenDonVi\":\"Phòng Công tác học sinh - sinh viên\",\"SoTin\":62,\"SoTinChuaXem\":41,\"LoaiDonVi\":\"PHONGBAN_TRUNGTAM\"},{\"MaDonVi\":\"P03\",\"TenDonVi\":\"Phòng Đại học\",\"SoTin\":223,\"SoTinChuaXem\":176,\"LoaiDonVi\":\"PHONGBAN_TRUNGTAM\"},{\"MaDonVi\":\"P07\",\"TenDonVi\":\"Phòng Khảo thí và KĐCL\",\"SoTin\":9,\"SoTinChuaXem\":4,\"LoaiDonVi\":\"PHONGBAN_TRUNGTAM\"},{\"MaDonVi\":\"P04\",\"TenDonVi\":\"Phòng Sau đại học\",\"SoTin\":2,\"SoTinChuaXem\":1,\"LoaiDonVi\":\"PHONGBAN_TRUNGTAM\"},{\"MaDonVi\":\"P09\",\"TenDonVi\":\"Phòng Tài chính\",\"SoTin\":28,\"SoTinChuaXem\":14,\"LoaiDonVi\":\"PHONGBAN_TRUNGTAM\"},{\"MaDonVi\":\"P15\",\"TenDonVi\":\"TDT Creative Language Center\",\"SoTin\":34,\"SoTinChuaXem\":11,\"LoaiDonVi\":\"KHOA\"},{\"MaDonVi\":\"P35\",\"TenDonVi\":\"TT Hợp tác doanh nghiệp và cựu sinh viên\",\"SoTin\":2,\"SoTinChuaXem\":2,\"LoaiDonVi\":\"PHONGBAN_TRUNGTAM\"},{\"MaDonVi\":\"P27\",\"TenDonVi\":\"Trung tâm đào tạo phát triển xã hội (SDTC)\",\"SoTin\":17,\"SoTinChuaXem\":9,\"LoaiDonVi\":\"PHONGBAN_TRUNGTAM\"},{\"MaDonVi\":\"G\",\"TenDonVi\":\"Trung tâm tin học\",\"SoTin\":21,\"SoTinChuaXem\":11,\"LoaiDonVi\":\"KHOA\"},{\"MaDonVi\":\"P48\",\"TenDonVi\":\"Viện chính sách kinh tế và kinh doanh (IBEP)\",\"SoTin\":2,\"SoTinChuaXem\":0,\"LoaiDonVi\":\"PHONGBAN_TRUNGTAM\"},{\"MaDonVi\":\"P02\",\"TenDonVi\":\"Phòng CTHSSV\",\"SoTin\":62,\"SoTinChuaXem\":41,\"LoaiDonVi\":\"PHONGBAN_TRUNGTAM\"},{\"MaDonVi\":\"P12\",\"TenDonVi\":\"Ban Ký túc xá\",\"SoTin\":1,\"SoTinChuaXem\":1,\"LoaiDonVi\":\"PHONGBAN_TRUNGTAM\"},{\"MaDonVi\":\"P35\",\"TenDonVi\":\"Trung tâm hợp tác doanh nghiệp và cựu sinh viên (CECA)\",\"SoTin\":2,\"SoTinChuaXem\":2,\"LoaiDonVi\":\"PHONGBAN_TRUNGTAM\"},{\"MaDonVi\":\"P07\",\"TenDonVi\":\"Phòng Khảo thí và kiểm định chất lượng\",\"SoTin\":9,\"SoTinChuaXem\":4,\"LoaiDonVi\":\"PHONGBAN_TRUNGTAM\"}]"',
-            false, 512, JSON_THROW_ON_ERROR), false, 512, JSON_THROW_ON_ERROR);
-        foreach ($faculties as $faculty) {
-            if ($faculty->TenDonVi === $value) {
-                return $faculty->MaDonVi;
-            }
+        switch (strtolower($value)) {
+
+            case 'khoa công nghệ thông tin':
+                return '5';
+            case 'khoa tài chính ngân hàng':
+                return 'B';
+            case 'trung tâm tin học':
+                return 'G';
+            case 'phòng cthssv':
+            case 'phòng công tác học sinh - sinh viên':
+                return 'P02';
+            case 'phòng đại học':
+                return 'P03';
+            case 'phòng sau đại học':
+                return 'P04';
+            case 'phòng khảo thí và kiểm định chất lượng':
+            case 'phòng khảo thí và kđcl':
+                return 'P07';
+            case 'phòng tài chính':
+                return 'P09';
+            case 'ban ký túc xá':
+                return 'P12';
+            case 'tdt creative language center':
+                return 'P15';
+            case 'trung tâm đào tạo phát triển xã hội (sdtc)':
+                return 'P27';
+            case 'trung tâm hợp tác doanh nghiệp và cựu sinh viên (ceca)':
+            case 'tt hợp tác doanh nghiệp và cựu sinh viên':
+                return 'P35';
+            case 'viện chính sách kinh tế và kinh doanh (ibep)':
+                return 'P48';
         }
     }
 }
