4fc4ba4bdbd2d735a005a074c7273554e7ec1442
Bùi Hữu Lộc <feature451@gmail.com>
2022-10-10 10:36:33 +0700

    fix: chart mail

--- resources/views/app/admin/statistic/chart_mail.blade.php
+++ resources/views/app/admin/statistic/chart_mail.blade.php
@@ -21,9 +21,6 @@
                         <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
                             <div class="report-box zoom-in">
                                 <div class="box p-5">
-                                    <div class="flex">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" icon-name="activity" data-lucide="activity" class="lucide lucide-activity report-box__icon text-primary"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 002 1.61h9.72a2 2 0 002-1.61L23 6H6"></path></svg>
-                                    </div>
                                     <div id="count_mail_this_week" class="text-3xl font-medium leading-8 mt-6"></div>
                                     <div class="text-base text-slate-500 mt-1">Số mail đã gửi trong tuần này</div>
                                 </div>
@@ -32,9 +29,6 @@
                         <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
                             <div class="report-box zoom-in">
                                 <div class="box p-5">
-                                    <div class="flex">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" icon-name="activity" data-lucide="activity" class="lucide lucide-activity report-box__icon text-primary"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 002 1.61h9.72a2 2 0 002-1.61L23 6H6"></path></svg>
-                                    </div>
                                     <div id="count_mail_this_month" class="text-3xl font-medium leading-8 mt-6"></div>
                                     <div class="text-base text-slate-500 mt-1">Số mail đã gửi trong tháng 10</div>
                                 </div>
@@ -43,9 +37,6 @@
                         <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
                             <div class="report-box zoom-in">
                                 <div class="box p-5">
-                                    <div class="flex">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" icon-name="activity" data-lucide="activity" class="lucide lucide-activity report-box__icon text-primary"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 002 1.61h9.72a2 2 0 002-1.61L23 6H6"></path></svg>
-                                    </div>
                                     <div id="count_mail_this_year" class="text-3xl font-medium leading-8 mt-6"></div>
                                     <div class="text-base text-slate-500 mt-1">Số mail đã gửi trong năm 2022</div>
                                 </div>
@@ -54,9 +45,6 @@
                         <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
                             <div class="report-box zoom-in">
                                 <div class="box p-5">
-                                    <div class="flex">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" icon-name="activity" data-lucide="activity" class="lucide lucide-activity report-box__icon text-primary"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 002 1.61h9.72a2 2 0 002-1.61L23 6H6"></path></svg>
-                                    </div>
                                     <div id="count_all" class="text-3xl font-medium leading-8 mt-6"></div>
                                     <div class="text-base text-slate-500 mt-1">Tổng số mail đã gửi từ trước tới giờ</div>
                                 </div>
@@ -120,52 +108,7 @@
                         'rgb(75, 192, 192)',
                         'rgb(54, 162, 235)',
                     ]
-                    const options = {
-                        maintainAspectRatio: false,
-                        plugins : {
-                            datalabels: {
-                                anchor: 'end',
-                                align: 'top',
-                                formatter: Math.round,
-                                font: {
-                                    weight: 'bold',
-                                    size: 16
-                                }
-                            },
-                            legend: {
-                                display: true,
-                                labels: {
-                                    color: "blue",
-                                    font: {
-                                        size: 24
-                                    },
-                                }
-                            },
-                        },
-                        scales: {
-                            y: {
-                                ticks: {
-                                    color: "yellow",
-                                    font: {
-                                        size: 18,
-                                    },
-                                    stepSize: 1,
-                                    beginAtZero: true,
-                                },
-                                // suggestedMax: data[3][1]
-                            },
-                            x: {
-                                ticks: {
-                                    color: "yellow",
-                                    font: {
-                                        size: 18
-                                    },
-                                    stepSize: 1,
-                                    beginAtZero: true,
-                                },
-                            }
-                        }
-                    }
+
 console.log(data)
                     // Các div thống kê
                     $('#count_mail_this_week').text(data[2][1])
@@ -174,15 +117,15 @@ console.log(data)
                     $('#count_all').text(data[5][1])
 
                     // Biểu đồ thống kê
-                    mailByMonths(data, background_color, border_color, options)
-                    mailByDays(data, background_color, border_color, options)
-                    usersReceiveMail(data, background_color, border_color, options)
+                    mailByMonths(data, background_color, border_color)
+                    mailByDays(data, background_color, border_color)
+                    usersReceiveMail(data, background_color, border_color)
                 }
             });
         })
     })
 
-    function mailByMonths(data, background_color, border_color, options)
+    function mailByMonths(data, background_color, border_color)
     {
         let mail_by_months = Object.entries(data[0][1])
         let data_sets = []
@@ -202,15 +145,15 @@ console.log(data)
         const config_chart_mails_by_month = {
             type: 'bar',
             data: data_chart_mails_by_month,
-            options: options,
+            options: getOptions(Math.max(...mail_by_months[0][1])),
         };
         new Chart($('#chart_mails_by_month'), config_chart_mails_by_month);
     }
 
-    function mailByDays(data, background_color, border_color, options)
+    function mailByDays(data, background_color, border_color)
     {
         const data_chart_mails_by_day = {
-            labels: [1, 2, 3, 4, 5, 6, 7].map(function (day) {return 'Thứ ' + day}),
+            labels: [2, 3, 4, 5, 6, 7, 8].map(function (day) {return 'Thứ ' + day}),
             datasets: [{
                 label: 'Tổng số tin nhắn đã gửi theo ngày trong tuần',
                 data: data[1][1],
@@ -222,12 +165,12 @@ console.log(data)
         const config_chart_mails_by_day = {
             type: 'bar',
             data: data_chart_mails_by_day,
-            options: options,
+            options: getOptions(Math.max(...data[1][1])),
         };
         new Chart($('#chart_mails_by_day'), config_chart_mails_by_day);
     }
 
-    function usersReceiveMail(data, background_color, border_color, options)
+    function usersReceiveMail(data, background_color, border_color)
     {
         const data_chart_users_receive_mail = {
             labels: data[6][1][0],
@@ -242,8 +185,60 @@ console.log(data)
         const config_chart_users_receive_mail = {
             type: 'bar',
             data: data_chart_users_receive_mail,
-            options: options
+            options: getOptions(Math.max(...data[6][1][1])),
         };
         new Chart($('#chart_users_receive_mail'), config_chart_users_receive_mail);
+    }
+
+    function getOptions(max_x_row)
+    {
+        console.log(max_x_row)
+        console.log(max_x_row + 10)
+        return {
+            maintainAspectRatio: false,
+            plugins : {
+                datalabels: {
+                    anchor: 'end',
+                    align: 'top',
+                    formatter: Math.round,
+                    font: {
+                        weight: 'bold',
+                        size: 16
+                    }
+                },
+                legend: {
+                    display: true,
+                    labels: {
+                        color: "blue",
+                        font: {
+                            size: 24
+                        },
+                    }
+                },
+            },
+            scales: {
+                y: {
+                    ticks: {
+                        color: "yellow",
+                        font: {
+                            size: 18,
+                        },
+                        stepSize: 1,
+                        beginAtZero: true,
+                    },
+                    suggestedMax: max_x_row + 10
+                },
+                x: {
+                    ticks: {
+                        color: "yellow",
+                        font: {
+                            size: 18
+                        },
+                        stepSize: 1,
+                        beginAtZero: true,
+                    },
+                }
+            }
+        }
     }
 </script>
